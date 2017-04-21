f5d4c4c08b727eae40dc9725a70dfe34cb2d7576
Chris Sawczuk <gummybassist@gmail.com>
2017-04-21 22:10:52 +0100

    Decouple wren_script_vm from script_context implementation

--- wrenvm/test/wren_script_vm.t.cpp
+++ wrenvm/test/wren_script_vm.t.cpp
@@ -2,26 +2,52 @@
 #include <wren_script_vm.h>
 #include <map>
 
-class mock_osd : public osd_renderer {
+class mock_script_services : public script_services {
 public:
-    void display_message(const std::string &msg, uint64_t duration) override {
-        printed_message = msg;
+    void print_string(const std::string &message) override {
+        printed_message = message;
     }
 
-    void add_rect(const osd_rect &rect) override {
-        captured_rect = rect;
+    void set_16bit_value(uint32_t address, uint16_t value) override {
+        stored16bit.first = address;
+        stored16bit.second = value;
     }
 
-    void add_image(const osd_image &image) override {
-        captured_image_file = image.name();
-        captured_image_x = image.x();
-        captured_image_y = image.y();
+    void set_8bit_value(uint32_t address, uint8_t value) override {
+        stored8bit.first = address;
+        stored8bit.second = value;
     }
 
-    void add_text(const std::string &text, int16_t x, int16_t y) override {
-        captured_text = text;
+    uint8_t read_8bit_value(uint32_t address) override {
+        return readable8bitValues[address];
+    }
+
+    uint16_t read_16bit_value(uint32_t address) override {
+        return readable16bitValues[address];
+    }
+
+    void add_image(const std::string &name, int16_t x, int16_t y) override {
+        captured_image_file = name;
+        captured_image_x = x;
+        captured_image_y = y;
+    }
+
+    void add_rect(int16_t x, int16_t y, int16_t w, int16_t h, uint32_t stroke, uint32_t fill) override {
+        captured_rect = osd_rect{x, y, w, h, stroke, fill};
+    }
+
+    void add_text(const std::string &message, int16_t x, int16_t y) override {
+        captured_text = message;
         captured_text_x = x;
         captured_text_y = y;
+    }
+
+    void queue_key(uint8_t key, uint32_t when, uint32_t duration) override {
+        captured_event = input_event{key, when, duration};
+    }
+
+    void register_command(const std::string &cmdName, script_command command) override {
+        _registeredCommands[cmdName] = command;
     }
 
     std::string printed_message;
@@ -34,36 +60,11 @@ public:
     std::string captured_image_file;
     int16_t captured_image_x;
     int16_t captured_image_y;
-};
-
-class mock_input_queue : public input_queue {
-public:
-    void queue_key(const input_event &event) override {
-        captured_event = event;
-    }
 
     input_event captured_event{0u, 0u, 0u};
-};
 
-class mock_memory_bridge : public memory_bridge {
-public:
-    uint8_t read_8bit(uint32_t addr) override {
-        return readable8bitValues[addr];
-    }
+    std::map<std::string, script_context::script_command> _registeredCommands;
 
-    void write_8bit(uint32_t addr, uint8_t val) override {
-        stored8bit.first = addr;
-        stored8bit.second = val;
-    }
-
-    uint16_t read_16bit(uint32_t addr) override {
-        return readable16bitValues[addr];
-    }
-
-    void write_16bit(uint32_t addr, uint16_t val) override {
-        stored16bit.first = addr;
-        stored16bit.second = val;
-    }
     std::map<uint32_t, uint8_t> readable8bitValues;
     std::map<uint32_t, uint16_t> readable16bitValues;
 
@@ -73,17 +74,9 @@ public:
 
 class WrenVMTest : public ::testing::Test {
 public:
-    mock_osd mockOsd;
-    mock_input_queue mockInputQueue;
-    mock_memory_bridge mockMemoryBridge;
+    mock_script_services scriptServices;
 
-    std::map<std::string, script_context::script_command> _registeredCommands;
-
-    script_context _ctx{&mockOsd, &mockInputQueue, &mockMemoryBridge, [&](const std::string &commandName, script_context::script_command command) {
-        _registeredCommands[commandName] = command;
-    }};
-
-    wren_script_vm vm{&_ctx};
+    wren_script_vm vm{&scriptServices};
 };
 
 TEST_F(WrenVMTest, canLoadEmptyScript) {
@@ -96,7 +89,7 @@ TEST_F(WrenVMTest, LuaVM_CanInvokeOnLoadFunction) {
                           "   GameBoy.print(\"Expected Message\")\n"
                           "}\n");
 
-    EXPECT_EQ("Expected Message", mockOsd.printed_message);
+    EXPECT_EQ("Expected Message", scriptServices.printed_message);
 }
 
 TEST_F(WrenVMTest, LuaVM_CanInvokeTickFunction) {
@@ -107,7 +100,7 @@ TEST_F(WrenVMTest, LuaVM_CanInvokeTickFunction) {
 
     vm.tick();
 
-    EXPECT_EQ("Expected Message", mockOsd.printed_message);
+    EXPECT_EQ("Expected Message", scriptServices.printed_message);
 }
 
 TEST_F(WrenVMTest, LuaVM_CanInvokeActivateFunction) {
@@ -118,7 +111,7 @@ TEST_F(WrenVMTest, LuaVM_CanInvokeActivateFunction) {
 
     vm.activate();
 
-    EXPECT_EQ("Expected Message", mockOsd.printed_message);
+    EXPECT_EQ("Expected Message", scriptServices.printed_message);
 }
 
 
@@ -131,7 +124,7 @@ TEST_F(WrenVMTest, LuaVM_CanHandleUnhandledCommands) {
 
     bool result = vm.handleUnhandledCommand("test_command", {"one"});
     EXPECT_TRUE(result);
-    EXPECT_EQ("Got command: test_command args: one", mockOsd.printed_message);
+    EXPECT_EQ("Got command: test_command args: one", scriptServices.printed_message);
 }
 
 TEST_F(WrenVMTest, LuaVM_CanIndicatedInabilityToHandleUnhandledCommands) {
@@ -148,68 +141,68 @@ TEST_F(WrenVMTest, LuaVM_CanIndicatedInabilityToHandleUnhandledCommands) {
 TEST_F(WrenVMTest, WrenVM_CanAddRect) {
     vm.loadScript("GameBoy.addRect(10, 20, 30, 40, 0xff00ff00, 0x00ff00ff)\n");
 
-    EXPECT_EQ(10, mockOsd.captured_rect.x());
-    EXPECT_EQ(20, mockOsd.captured_rect.y());
-    EXPECT_EQ(30u, mockOsd.captured_rect.w());
-    EXPECT_EQ(40u, mockOsd.captured_rect.h());
-    EXPECT_EQ(0xff00ff00u, mockOsd.captured_rect.stroke());
-    EXPECT_EQ(0x00ff00ffu, mockOsd.captured_rect.fill());
+    EXPECT_EQ(10, scriptServices.captured_rect.x());
+    EXPECT_EQ(20, scriptServices.captured_rect.y());
+    EXPECT_EQ(30u, scriptServices.captured_rect.w());
+    EXPECT_EQ(40u, scriptServices.captured_rect.h());
+    EXPECT_EQ(0xff00ff00u, scriptServices.captured_rect.stroke());
+    EXPECT_EQ(0x00ff00ffu, scriptServices.captured_rect.fill());
 }
 
 TEST_F(WrenVMTest, WrenVM_CanAddText) {
     vm.loadScript("GameBoy.addText(\"Expected Text\", 10, 20)\n");
 
-    EXPECT_EQ("Expected Text", mockOsd.captured_text);
-    EXPECT_EQ(10, mockOsd.captured_text_x);
-    EXPECT_EQ(20, mockOsd.captured_text_y);
+    EXPECT_EQ("Expected Text", scriptServices.captured_text);
+    EXPECT_EQ(10, scriptServices.captured_text_x);
+    EXPECT_EQ(20, scriptServices.captured_text_y);
 }
 
 
 TEST_F(WrenVMTest, WrenVM_CanAddImage) {
     vm.loadScript("GameBoy.addImage(\"image_file.png\", 10, 20)\n");
 
-    EXPECT_EQ("image_file.png", mockOsd.captured_image_file);
-    EXPECT_EQ(10, mockOsd.captured_image_x);
-    EXPECT_EQ(20, mockOsd.captured_image_y);
+    EXPECT_EQ("image_file.png", scriptServices.captured_image_file);
+    EXPECT_EQ(10, scriptServices.captured_image_x);
+    EXPECT_EQ(20, scriptServices.captured_image_y);
 }
 
 
 TEST_F(WrenVMTest, WrenVM_CanQueueInputEvents) {
     vm.loadScript("GameBoy.queueKey(0x01, 100, 200)\n");
 
-    EXPECT_EQ(0x01u, mockInputQueue.captured_event.key);
-    EXPECT_EQ(100u, mockInputQueue.captured_event.when);
-    EXPECT_EQ(200u, mockInputQueue.captured_event.duration);
+    EXPECT_EQ(0x01u, scriptServices.captured_event.key);
+    EXPECT_EQ(100u, scriptServices.captured_event.when);
+    EXPECT_EQ(200u, scriptServices.captured_event.duration);
 }
 
 TEST_F(WrenVMTest, WrenVM_CanRead8bitValue) {
-    mockMemoryBridge.readable8bitValues[0x1234] = 128u;
+    scriptServices.readable8bitValues[0x1234] = 128u;
 
     vm.loadScript("GameBoy.print(\"Val: %(GameBoy.get8bit(0x1234))\")\n");
 
-    EXPECT_EQ("Val: 128", mockOsd.printed_message);
+    EXPECT_EQ("Val: 128", scriptServices.printed_message);
 }
 
 TEST_F(WrenVMTest, WrenVM_CanRead16bitValue) {
-    mockMemoryBridge.readable16bitValues[0x123] = 32768u;
+    scriptServices.readable16bitValues[0x123] = 32768u;
 
     vm.loadScript("GameBoy.print(\"Val: %(GameBoy.get16bit(0x123))\")\n");
 
-    EXPECT_EQ("Val: 32768", mockOsd.printed_message);
+    EXPECT_EQ("Val: 32768", scriptServices.printed_message);
 }
 
 TEST_F(WrenVMTest, WrenVM_CanWrite8bitValue) {
     vm.loadScript("GameBoy.set8bit(0x1234, 128)\n");
 
-    EXPECT_EQ(0x1234u, mockMemoryBridge.stored8bit.first);
-    EXPECT_EQ(128u, mockMemoryBridge.stored8bit.second);
+    EXPECT_EQ(0x1234u, scriptServices.stored8bit.first);
+    EXPECT_EQ(128u, scriptServices.stored8bit.second);
 }
 
 TEST_F(WrenVMTest, WrenVM_CanWrite16bitValue) {
     vm.loadScript("GameBoy.set16bit(0x123, 32768)\n");
 
-    EXPECT_EQ(0x123u, mockMemoryBridge.stored16bit.first);
-    EXPECT_EQ(32768u, mockMemoryBridge.stored16bit.second);
+    EXPECT_EQ(0x123u, scriptServices.stored16bit.first);
+    EXPECT_EQ(32768u, scriptServices.stored16bit.second);
 }
 
 TEST_F(WrenVMTest, WrenVM_CanRegisterConsoleCommands) {
@@ -218,9 +211,9 @@ TEST_F(WrenVMTest, WrenVM_CanRegisterConsoleCommands) {
             "    GameBoy.print(\"Expected Func %(args[0])\")\n"
             "})\n");
 
-    EXPECT_TRUE(_registeredCommands.find("cfunc") != _registeredCommands.end());
+    EXPECT_TRUE(scriptServices._registeredCommands.find("cfunc") != scriptServices._registeredCommands.end());
 
-    _registeredCommands["cfunc"]({"arg"});
+    scriptServices._registeredCommands["cfunc"]({"arg"});
 
-    EXPECT_EQ("Expected Func arg", mockOsd.printed_message);
+    EXPECT_EQ("Expected Func arg", scriptServices.printed_message);
 }
