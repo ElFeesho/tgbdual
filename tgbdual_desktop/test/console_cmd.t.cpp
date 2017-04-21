43338fcdfa1823e0472c904cbcbf0eb037d4cb94
Chris Sawczuk <gummybassist@gmail.com>
2017-04-21 23:05:49 +0100

    Add coverage reporting

--- tgbdual_desktop/test/console_cmd.t.cpp
+++ tgbdual_desktop/test/console_cmd.t.cpp
@@ -1,16 +1,12 @@
-//
-// Created by chris on 02/04/17.
-//
-
 #include <gtest/gtest.h>
 #include <functional>
 #include <console/console_cmd.h>
 
 TEST(can_invoke_cmd_with_no_args, like_void) {
     bool executed = false;
-    console_cmd testCommand{"test", [&](std::vector<std::string> args) {
+    console_cmd testCommand{[&](std::vector<std::string> args) {
         executed = true;
-    }}(console_cmd::ConsoleCallback());
+    }};
 
     testCommand.invoke();
 
@@ -21,12 +17,12 @@ TEST(can_invoke_cmd_with_args, like_two_ints_and_a_string) {
     int one, two;
     std::string three;
 
-    console_cmd testCommand{"test", [&](std::vector<std::string> args) {
+    console_cmd testCommand{[&](std::vector<std::string> args) {
         one = console_cmd::fromDec(args[0]);
         two = console_cmd::fromDec(args[1]);
         two = std::atoi(args[1].c_str());
         three = args[2];
-    }}(console_cmd::ConsoleCallback());
+    }};
 
     testCommand.invoke("10 20 three");
 
@@ -38,11 +34,10 @@ TEST(can_invoke_cmd_with_args, like_two_ints_and_a_string) {
 TEST(can_invoke_cmd_with_args, like_two_hex_ints) {
     int one, two;
 
-    console_cmd testCommand{"test", [&](std::vector<std::string> args) {
-        std::stringstream s;
+    console_cmd testCommand{[&](std::vector<std::string> args) {
         one = console_cmd::fromHex(args[0]);
         two = console_cmd::fromHex(args[1]);
-    }}(console_cmd::ConsoleCallback());
+    }};
 
     testCommand.invoke("0x10 0x20");
 
