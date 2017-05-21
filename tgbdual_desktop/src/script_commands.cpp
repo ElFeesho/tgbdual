4838fa4505ad62c2e711a1958c93f40f61309ff3
Chris Sawczuk <gummybassist@gmail.com>
2017-05-21 20:15:44 +0100

    Refactor script loading

--- tgbdual_desktop/src/script_commands.cpp
+++ tgbdual_desktop/src/script_commands.cpp
@@ -4,37 +4,52 @@
 #include <lua_script_vm.h>
 #include <io/file_buffer.h>
 
+enum class ScriptEngineType {
+    Lua,
+    Wren,
+    Unknown
+};
+
+ScriptEngineType scriptEngineTypeForFile(const std::string &file) {
+    if (file.find(".wren") != std::string::npos) {
+        return ScriptEngineType::Wren;
+    }
+
+    if (file.find(".lua") != std::string::npos) {
+        return ScriptEngineType::Lua;
+    }
+
+    return ScriptEngineType::Unknown;
+}
+
+script_vm *createVmForType(tgbdual &tgb, ScriptEngineType type) {
+    if (type == ScriptEngineType::Wren) {
+        return new wren_script_vm{tgb.getScriptServices()};
+    }
+
+    return new lua_script_vm{tgb.getScriptServices()};
+}
+
 void registerScriptCommands(tgbdual &tgb) {
     tgb.addConsoleCommand("load_script", [&](std::vector<std::string> args) {
         if (args.size() == 1) {
             std::string &file = args[0];
-            if (file.find(".wren") != std::string::npos) {
 
-                wren_script_vm *wrenVm = new wren_script_vm(tgb.getScriptServices());
-                try {
-                    wrenVm->loadScript(file_buffer{file});
-                    tgb.removeVm(file);
-                    tgb.addVm(file, wrenVm);
-                    tgb.addConsoleOutput("Loaded " + file + " wren script");
-                }
-                catch (std::domain_error &e) {
-                    tgb.addConsoleErrorOutput(e.what());
-                    delete wrenVm;
-                }
-            } else if (file.find(".lua") != std::string::npos) {
-                lua_script_vm *luaVm = new lua_script_vm(tgb.getScriptServices());
-                try {
-                    luaVm->loadScript((file_buffer{file}));
-                    tgb.removeVm(file);
-                    tgb.addVm(file, luaVm);
-                    tgb.addConsoleOutput("Loaded " + file + " lua script");
-                }
-                catch (std::domain_error &e) {
-                    tgb.addConsoleErrorOutput(e.what());
-                    delete luaVm;
-                }
-            } else {
+            ScriptEngineType type = scriptEngineTypeForFile(file);
+            if (type == ScriptEngineType::Unknown) {
                 tgb.addConsoleErrorOutput("Cannot load " + file);
+            } else {
+                script_vm *vm = createVmForType(tgb, type);
+                try {
+                    vm->loadScript(file_buffer{file});
+                    tgb.removeVm(file);
+                    tgb.addVm(file, vm);
+                    tgb.addConsoleOutput("Loaded script: " + file);
+                }
+                catch (std::domain_error &e) {
+                    tgb.addConsoleErrorOutput(e.what());
+                    delete vm;
+                }
             }
         } else {
             tgb.addConsoleErrorOutput("Usage: load_script [file]");
